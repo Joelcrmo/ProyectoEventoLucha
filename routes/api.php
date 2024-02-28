6777fe0fa40eacf0782181e7059753d1f4b13811
joelcrmo <joeljudogc@gmail.com>
2024-02-28 20:07:37 +0000

    se modifica ruta route de la api

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,7 @@ Route::group(['prefix' => 'joel'], function () {
 
     Route::post('Pelea', [PeleaController::class, 'store']);
     Route::post('Velada', [VeladaController::class, 'store']);
+    Route::post('Validacion', [ValidacionController::class, 'store']);
 
 
  });
